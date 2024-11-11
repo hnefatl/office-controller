86ed42a93b17cba1c50023692eee16565d2db82e
Keith <hnefatl@gmail.com>
2024-11-11 22:08:13 +0000

    Fancy 'only rebuild build script if some configs change'

--- build.rs
+++ build.rs
@@ -1,4 +1,11 @@
 fn main() {
+    // Only rerun this build script if these files change: avoids
+    // rebuilds for binary code.
+    println!("cargo::rerun-if-changed=build.rs");
+    println!("cargo::rerun-if-changed=Cargo.toml");
+    println!("cargo::rerun-if-changed=config/");
+    println!("cargo::rerun-if-changed=deployment_config.toml");
+
     // Verify the config can be parsed at build-time, to prevent config parsing errors at runtime.
     config::Config::load_or_panic();
 
