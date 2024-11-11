5489fec8e32712a9420b576cbddb139ef2f04758
Keith <hnefatl@gmail.com>
2024-11-11 20:48:57 +0000

    Switch to async style with tasks for each functionality.

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,9 @@
-#![no_main]
-
 use anyhow::Result;
-use esp_idf_svc::{
-    eventloop::EspSystemEventLoop,
-    hal::{delay::Delay, prelude::Peripherals},
-    nvs::EspDefaultNvsPartition,
-};
+use embassy_executor::{main, Spawner};
+use embassy_time::Timer;
+use esp_idf_svc::{eventloop::EspSystemEventLoop, hal::prelude::*, nvs::EspDefaultNvsPartition};
 use log::info;
-use wifi::Wifi;
+use std::sync::Arc;
 
 mod homeassistant;
 mod wifi;
@@ -15,60 +11,65 @@ mod wifi;
 // Disabled until I've got NVS encryption configured, don't want to leak WiFi keys via flash.
 const USE_PERSISTENT_WIFI_STORAGE: bool = false;
 
-#[no_mangle]
-fn main() -> Result<()> {
+#[main]
+async fn main(spawner: Spawner) {
     let config = config::Config::load_or_panic();
 
     esp_idf_svc::sys::link_patches();
     esp_idf_svc::log::EspLogger::initialize_default();
 
     let peripherals = Peripherals::take().unwrap();
-    let sysloop = EspSystemEventLoop::take()?;
+    let sysloop = EspSystemEventLoop::take().unwrap();
 
-    // Allow storing wifi tuning data and keys(?) in persistent storage, for better performance.
+    // Allow storing wifi tuning data and keys(?) in persistent storage, for better performance(?).
+    // It at least stops an error on boot, although it's not runtime-critical.
     let wifi_nvs = USE_PERSISTENT_WIFI_STORAGE
         .then(EspDefaultNvsPartition::take)
-        .transpose()?;
+        .transpose()
+        .unwrap();
 
-    let mut wifi = wifi::Wifi::new(peripherals.modem, sysloop.clone(), wifi_nvs)?;
-    info!("Starting wifi");
-    wifi.start()?;
-
-    main_loop(config, &mut wifi);
+    let wifi_status = Arc::new(wifi::WifiStatus::new());
+    spawner.must_spawn(wifi::repeatedly_connect_to_wifi(
+        peripherals.modem,
+        sysloop.clone(),
+        wifi_nvs,
+        wifi_status.clone(),
+        config.networks,
+    ));
+    spawner.must_spawn(homeassistant_loop(
+        wifi_status.clone(),
+        config.home_assistant_config,
+    ));
 }
 
-fn main_loop(config: config::Config, wifi: &mut Wifi) -> ! {
-    let delay = Delay::new_default();
+#[embassy_executor::task]
+async fn homeassistant_loop(
+    wifi_status: Arc<wifi::WifiStatus>,
+    config: config::HomeAssistantConfig,
+) -> ! {
+    loop {
+        wifi_status.wait_until_connected().await;
 
-    for network in config.networks.iter().cycle() {
-        if let Err(e) = wifi.try_connect(network) {
-            info!("Failed to connect to wifi: {}", e);
-            delay.delay_ms(1000);
-            continue;
-        }
-
-        loop {
-            match homeassistant::StateSnapshot::get(&config.home_assistant_config) {
-                Ok(s) => match update(&config, s) {
-                    Ok(_) => {}
-                    Err(e) => {
-                        info!("Failed to run update: {}", e);
-                        break;
-                    }
-                },
+        match homeassistant::StateSnapshot::get(&config) {
+            Ok(s) => match update(&config, s) {
+                Ok(_) => {}
                 Err(e) => {
-                    info!("Failed to fetch HA state: {}", e);
-                    break;
+                    info!("Failed to run update: {}", e);
                 }
+            },
+            Err(e) => {
+                info!("Failed to fetch HA state: {}", e);
             }
-            delay.delay_ms(5000);
         }
+        Timer::after_secs(5).await;
     }
-    panic!("No network config provided")
 }
 
-fn update(config: &config::Config, state_snapshot: homeassistant::StateSnapshot) -> Result<()> {
-    if state_snapshot.person_location.state == config.home_assistant_config.person_entity {
+fn update(
+    config: &config::HomeAssistantConfig,
+    state_snapshot: homeassistant::StateSnapshot,
+) -> Result<()> {
+    if state_snapshot.person_location.state == config.person_entity {
         info!("update pin");
     } else {
         info!(
--- src/wifi.rs
+++ src/wifi.rs
@@ -1,63 +1,100 @@
+use embassy_sync::{blocking_mutex::raw::CriticalSectionRawMutex, mutex::Mutex};
+use embassy_time::{Duration, Timer};
 use esp_idf_svc::{
     eventloop::EspSystemEventLoop,
-    hal::{modem::WifiModemPeripheral, peripheral::Peripheral},
+    hal::modem::Modem,
     nvs::EspDefaultNvsPartition,
     sys::EspError,
-    wifi::{self, BlockingWifi, ClientConfiguration, EspWifi},
+    timer::EspTaskTimerService,
+    wifi::{self, AsyncWifi, ClientConfiguration, EspWifi},
 };
-
 use log::info;
+use std::sync::Arc;
 
-pub struct Wifi<'d> {
-    wifi: BlockingWifi<EspWifi<'d>>,
-}
-impl Wifi<'_> {
-    pub fn new<'d, M>(
-        modem: impl Peripheral<P = M> + 'd,
-        sysloop: EspSystemEventLoop,
-        nvs: Option<EspDefaultNvsPartition>,
-    ) -> Result<Wifi<'d>, EspError>
-    where
-        M: WifiModemPeripheral,
-    {
-        let esp_wifi = EspWifi::new(modem, sysloop.clone(), nvs)?;
-        let wifi = BlockingWifi::wrap(esp_wifi, sysloop.clone())?;
-        Ok(Wifi { wifi })
-    }
+#[embassy_executor::task]
+pub async fn repeatedly_connect_to_wifi(
+    modem: Modem,
+    sysloop: EspSystemEventLoop,
+    nvs: Option<EspDefaultNvsPartition>,
+    status: Arc<WifiStatus>,
+    networks: Vec<config::WifiNetwork>,
+) -> ! {
+    let esp_wifi = EspWifi::new(modem, sysloop.clone(), nvs).unwrap();
+    let timer = EspTaskTimerService::new().unwrap();
+    let mut wifi = AsyncWifi::wrap(esp_wifi, sysloop.clone(), timer).unwrap();
 
-    pub fn start(&mut self) -> Result<(), EspError> {
-        self.wifi.start()
-    }
-
-    pub fn try_connect(
-        &mut self,
-        network: &config::WifiNetwork,
-    ) -> std::result::Result<(), EspError> {
-        // Start fresh if already connected.
-        if self.wifi.is_connected()? {
-            info!("Already connected, disconnecting");
-            let _ = self.wifi.disconnect();
+    wifi.start().await.unwrap();
+    for network in networks.iter().cycle() {
+        if let Err(e) = try_connect(&mut wifi, network).await {
+            info!("Failed to connect to wifi: {}", e);
+            Timer::after(Duration::from_secs(1)).await;
+            continue;
         }
 
-        info!("Attempting connection to WiFi SSID '{}'", network.ssid);
-        self.wifi
-            .set_configuration(&wifi::Configuration::Client(ClientConfiguration {
-                ssid: network
-                    .ssid
-                    .parse()
-                    .expect("Could not parse the given SSID into WiFi config"),
-                password: network
-                    .password
-                    .parse()
-                    .expect("Could not parse the given password into WiFi config"),
-                channel: None, // Autodiscover the channel
-                auth_method: network.auth_method,
-                ..Default::default()
-            }))?;
-        self.wifi.connect()?;
-        self.wifi.wait_netif_up()?;
-        let ip_info = self.wifi.wifi().sta_netif().get_ip_info()?;
-        info!("Wifi DHCP info: {:?}", ip_info);
-        Ok(())
+        info!("Connected, waiting until disconnect");
+        status.set_connected(true).await;
+        wifi.wifi_wait(|w| w.is_connected(), None).await.unwrap();
+        status.set_connected(false).await;
+        info!("Disconnected");
+    }
+    panic!("No network config provided")
+}
+
+async fn try_connect(
+    wifi: &mut AsyncWifi<EspWifi<'_>>,
+    network: &config::WifiNetwork,
+) -> std::result::Result<(), EspError> {
+    // Start fresh if already connected.
+    if wifi.is_connected()? {
+        info!("Already connected, disconnecting");
+        let _ = wifi.disconnect().await;
+    }
+
+    info!("Attempting connection to WiFi SSID '{}'", network.ssid);
+    wifi.set_configuration(&wifi::Configuration::Client(ClientConfiguration {
+        ssid: network
+            .ssid
+            .parse()
+            .expect("Could not parse the given SSID into WiFi config"),
+        password: network
+            .password
+            .parse()
+            .expect("Could not parse the given password into WiFi config"),
+        channel: None, // Autodiscover the channel
+        auth_method: network.auth_method,
+        ..Default::default()
+    }))?;
+    wifi.connect().await?;
+    wifi.wait_netif_up().await?;
+    let ip_info = wifi.wifi().sta_netif().get_ip_info()?;
+    info!("Wifi DHCP info: {:?}", ip_info);
+    Ok(())
+}
+
+pub struct WifiStatus {
+    connected: Mutex<CriticalSectionRawMutex, bool>,
+}
+impl WifiStatus {
+    pub fn new() -> WifiStatus {
+        WifiStatus {
+            connected: Mutex::new(false),
+        }
+    }
+
+    /// "Best-effort" wait: blocks until connected has been set at least once, but doesn't guarantee
+    /// that it's still set at any point afterwards.
+    /// Intended to act as a soft block of "if we're clearly disconnected, don't do anything until
+    /// we're connected again".
+    pub async fn wait_until_connected(&self) {
+        while !*self.connected.lock().await {
+            // Polling is ugly but simple. The embassy sync primitives require knowing at compile-time
+            // how many tasks can wait at once, and that's more annoying to calculate and pass around
+            // in a clean manner than it is to just spinlock.
+            Timer::after_secs(1).await;
+        }
+    }
+
+    async fn set_connected(&self, connected: bool) {
+        *self.connected.lock().await = connected;
     }
 }
