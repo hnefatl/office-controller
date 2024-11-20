af4a5d13fafbbcc6f07b372ca901f043c7d6bcff
Keith <hnefatl@gmail.com>
2024-11-20 00:14:54 +0000

    Remove the "test config is valid at build time" magic. Causes rust-lsp crashes if the config doesn't match, which is common when switching branches. The config alone is fast to flash now so this should be quick to fix up.

--- build.rs
+++ build.rs
@@ -1,14 +1,3 @@
-const CONFIG: &str = include_str!("deployment_config.toml");
-
 fn main() {
-    // Only rerun this build script if these files change: avoids
-    // rebuilds for binary code.
-    println!("cargo::rerun-if-changed=build.rs");
-    println!("cargo::rerun-if-changed=Cargo.toml");
-    println!("cargo::rerun-if-changed=config/");
-    println!("cargo::rerun-if-changed=deployment_config.toml");
     embuild::espidf::sysenv::output();
-
-    // Verify the config can be parsed at build-time, to prevent config parsing errors at runtime.
-    config::Config::parse_or_panic(CONFIG);
 }
